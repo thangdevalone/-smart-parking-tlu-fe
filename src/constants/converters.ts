432a4ba37f1ec2821d429561a76053499f72d438
ThangDevAlone <quangthangvtlg@gmail.com>
2024-09-06 00:53:10 +0700

    :wrench: Add converter language

---
+++ src/constants/converters.ts
@@ -0,0 +1,7 @@
+export const ConvertColumnIDs: { [key: string]: string } = {
+  'id': 'Mã',
+  'cardTypeName': 'Tên thẻ',
+  'cardTypePrice': 'Giá',
+  'createdAt': 'Tạo lúc',
+  'updatedAt': 'Sửa lúc',
+};
