3613f16fc6ce395235923e973baa73191cf5c8ae
Thắng Nguyễn <thangdevalone@gmail.com>
2024-11-27 21:58:31 +0700

    :bug: fix UI

--- src/views/manager-history/table/history-columns.tsx
+++ src/views/manager-history/table/history-columns.tsx
@@ -65,7 +65,7 @@ export const historyColumns: ColumnDef<History>[] = [
     ),
     cell: ({ row }) => {
       const bill = row.getValue('price') as any;
-      return <div>{bill ? CurrencyFormatter.toVND(bill.price || '3000') : 'N/A'}</div>;
+      return <div>{bill ? CurrencyFormatter.toVND(bill.price || 0) : 'N/A'}</div>;
     },
   },
   {
@@ -75,7 +75,7 @@ export const historyColumns: ColumnDef<History>[] = [
     ),
     cell: ({ row }) => {
       const bill = row.getValue('price') as any;
-      return <div>{+bill? 'Vé ngày' : 'Vé tháng'}</div>;
+      return <div>{+bill ? 'Vé ngày' : 'Vé tháng'}</div>;
     },
   },
 ];
